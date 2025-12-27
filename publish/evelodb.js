45931a5adea3b620cf58bd840cdf45165ca9b551
Evelocore <kumuthuprabhasha@gmail.com>
2025-12-27 13:42:52 +0530

    update -v

--- publish/evelodb.js
+++ publish/evelodb.js
@@ -686,7 +686,7 @@ class eveloDB {
                     // Compare only user-provided fields
                     return Object.keys(data).every(key => {
                         // Skip comparison if this is an auto-generated field
-                        if (key === '__id') return true;
+                        if (key === this.config.autoPrimaryKey) return true;
                         return deepCompare(existingItem[key], data[key]);
                     }) &&
                         // Also ensure we're not matching against records missing compared fields
@@ -979,12 +979,12 @@ class eveloDB {
 
                 if (this.config.noRepeat) {
                     const isDuplicate = db.some(existingItem => {
-                        if (existingItem.__id && item.__id && existingItem.__id === item.__id) {
+                        if (existingItem[this.config.autoPrimaryKey] && item[this.config.autoPrimaryKey] && existingItem[this.config.autoPrimaryKey] === item[this.config.autoPrimaryKey]) {
                             return false;
                         }
 
                         return Object.keys(newData).every(key => {
-                            if (key === '__id') return false;
+                            if (key === this.config.autoPrimaryKey) return false;
                             return deepCompare(existingItem[key], updatedItem[key]);
                         });
                     });
